16ae5e0e7aee894bde86b72947986981c4c72340
im-bipo <bipinkhatri.ram@gmail.com>
2024-04-15 22:25:51 +0545

    SETUP: Mongo DB with Prisma

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from "react";
+import { PrismaClient } from "@prisma/client";
 
 const App = () => {
-  return (
-    <div>
-      App
-    </div>
-  )
-}
+  const prisma = new PrismaClient();
 
-export default App
+  async function main() {
+    // const users = await prisma.users.findMany()
+    // console.log(users)
+    const user = await prisma.user.create({
+      data: {
+        email: "wow@gmail.com",
+        name: "hello",
+      },
+    });
+    console.log(user);
+  }
+  main()
+    .then(async () => {
+      await prisma.$disconnect();
+    })
+    .catch(async () => {
+      await prisma.$disconnect();
+      process.exit(1);
+    });
+
+  return <div>App</div>;
+};
+
+export default App;
